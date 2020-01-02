09a4a2d940603db5ef5dc3b3dd19d19985f8b7a1
robertsbaer <robertsbaer@yahoo.com>
2020-01-01 19:44:14 -0500

    fetc data completed setup script

--- fetchdata.js
+++ fetchdata.js
@@ -1,11 +1,23 @@
 const axios = require('axios')
-const DATA_API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.dsc'
+require('dotenv').config()
+
+const TOKEN = process.env.TOKEN
+const DATA_API_URL = process.env.DATA_API_URL || 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.dsc'
+const MY_API_URL = process.env.MY_API_URL || 'http://localhost:3000'
+
  
 axios({
     method: 'GET',
     url: DATA_API_URL,
-    //headers: { Authorization: `Bearer ${TOKEN}` }
+    headers: { Authorization: `Bearer ${TOKEN}` }
 }).then(res => {
-    const movie = res.data.results
-    console.log('axios.then movie', movie)
+    const movies = res.data.results
+    console.log('axios.then movies', movies)
+    movies.forEach(movie => {
+        axios({
+            method: 'POST',
+            url: `${MY_API_URL}/movies`,
+            data: movie
+        })
+    })
 }).catch(err => console.log('axios.catch err', err))
